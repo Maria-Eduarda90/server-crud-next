5ddff5a5701709ed75b434f86ff4eb9916ec1b9b
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-12-22 10:16:08 -0300

    chore: creating my entire table in my Vercel database

---
+++ src/app/api/create-table/route.ts
@@ -0,0 +1,12 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const result = await sql`CREATE TABLE todos (Text varchar(255))`;
+
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ err }, { status: 500 });
+  }
+}
