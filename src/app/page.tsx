16814550051f5bea4375ed50d196d0612f44f170
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-12-22 10:43:42 -0300

    chore: component to add everything imported to my main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import { sql } from "@vercel/postgres";
+import { AddForm } from "./components/add-form";
+
+
+export default async function Home() {
+  let data = await sql`SELECT *  FROM todos`;
+  const { rows: todos } = data;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <h1 className='text-white'>Hello</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
+      <h1 className=''>TODO</h1>
+
+      <AddForm />
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
