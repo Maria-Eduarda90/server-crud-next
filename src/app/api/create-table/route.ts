6b3d33d54e4770dfad491f22c186abd443a4afab
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-12-22 10:58:05 -0300

    feat: changing my query and adding a field to generate an id

--- src/app/api/create-table/route.ts
+++ src/app/api/create-table/route.ts
@@ -3,7 +3,8 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
-    const result = await sql`CREATE TABLE todos (Text varchar(255))`;
+    const result =
+      await sql`CREATE TABLE todos (id SERIAL PRIMARY KEY, text TEXT NOT NULL )`;
 
     return NextResponse.json({ result }, { status: 200 });
   } catch (err) {
